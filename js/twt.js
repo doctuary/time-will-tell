2ab7b0201d3f2c597f68d124773ccea1ddea22c7
Todd Gutschow <doctuary@outlook.com>
2017-06-09 14:01:46 -0500

    Create ipfs node in twt.js

--- js/twt.js
+++ js/twt.js
@@ -1,3 +1,25 @@
+const repoPath = 'ipfs-' + Math.random()
+// Create an IPFS node
+const node = new Ipfs({
+    init: false,
+    start: false,
+    repo: repoPath
+})
+// Init the node
+node.init(handleInit)
+function handleInit (err) {
+    if (err) {
+        throw err
+    }
+    node.start(() => {
+        console.log('Online status: ', node.isOnline() ? 'online' : 'offline')
+        document.getElementById("status").innerHTML= 'Node status: ' + (node.isOnline() ? 'online' : 'offline')
+        // You can write more code here to use it. Use methods like 
+        // node.files.add, node.files.get. See the API docs here:
+        // https://github.com/ipfs/interface-ipfs-core/tree/master/API
+    })
+}
+
 function saveJournalEntry() {
     console.log("Save successful");
     let status = document.getElementById("status");
